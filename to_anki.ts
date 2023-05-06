a4a5d48e00d6ed32ca464c7097809da99d18024e
Heechul Lim <skyde1021@dgist.ac.kr>
2023-05-06 12:31:40 +0900

    Bug fix (Highlighting)

--- to_anki.ts
+++ to_anki.ts
@@ -15,23 +15,21 @@ import {
 } from "obsidian";
 export class ToAnki {
 
-	getDiffRegex(a: string, b: string): string[] {
+	getDiffRegex(a: string, b: string): [string[], string[]] {
 		const diff = require('diff');
 		const diffResult = diff.diffWordsWithSpace(a, b);
 		let result = '';
-		let added_regex = "("
-		let removed_regex = "("
+		let added_array: string[] = []
+		let removed_array: string[] = []
 		diffResult.forEach(part => {
 			if (part.added) {
-				added_regex += `${part.value}|`
+				added_array = [...added_array, part.value]
 			} else if (part.removed) {
-				removed_regex += `${part.value}|`
+				removed_array = [...removed_array, part.value]
 			} else {
 			}
 		});
-		added_regex = added_regex.replace(/\|$/, ")")
-		removed_regex = removed_regex.replace(/\|$/, ")")
-		return [added_regex, removed_regex];
+		return [added_array, removed_array]
 	}
 
 	shuffleArray<T>(arr: T[]): T[] {
@@ -42,22 +40,42 @@ export class ToAnki {
 		return arr;
 	}
 
+	replaceMatchedStringsToGetColor(str: string, list: string[], color: string): string {
+		// 짧은 단어가 그 단어를 포함하는 긴 단어에 영향을 주지 않도록, list를 역순으로 정렬하여 가장 긴 부분부터 바꾸도록 함
+		list.sort((a, b) => b.length - a.length);
+
+		// 각 요소를 순회하며 해당하는 부분을 찾아서 A로 대체
+		for (let i = 0; i < list.length; i++) {
+			// 각 요소의 앞뒤 공백은 없어야 하며(trim), 앞뒤가 일반문자(a-zA-z0-9_)가 아니고(\W), string 의 맨앞 또는 뒤(^ or $) 이어야 하며, 또한 string 일때는 escape 까지 해서 \\W 로 적어야 함
+			let regex_str1 = `(^${list[i].trim()})` // 시작일 때
+			str = str.replace(new RegExp(regex_str1, 'g'), `<font color=${color}>$1</font>`);
+			let regex_str2 = `(\\W)(${list[i].trim()})(\\W)` // 중간일 때
+			str = str.replace(new RegExp(regex_str2, 'g'), `$1<font color=${color}>$2</font>$3`); // \\W 를 <> 의 밖에 둔다는 게 포인트인데, 만약 안에 두면 \\W 가 < 또는 > 일 때, HTML 코드가 꼬이기 때문
+			let regex_str3 = `(${list[i].trim()}$)`// 끝일 때
+			str = str.replace(new RegExp(regex_str3, 'g'), `<font color=${color}>$1</font>`);
+			console.log(`after ${str}`)
+		}
+
+		return str;
+	}
+
 
 	BuildAnkiFormat(question: string, answer: string) {
-		question = question.replace(/(\(\))/g, "")
-		answer = answer.replace(/(\(\))/g, "")
+		question = question.replace(/(\(|\))/g, "")
+		answer = answer.replace(/(\(|\))/g, "")
 
-		let [added_regex, removed_regex] = this.getDiffRegex(question, answer)
-		question = question.replace(new RegExp(removed_regex, "g"), "<font color=#cc0000>**$1**</font>")
+		let [added_array, removed_array] = this.getDiffRegex(question, answer)
+		question = this.replaceMatchedStringsToGetColor(question, removed_array, "#cc0000")
+		answer = this.replaceMatchedStringsToGetColor(answer, added_array, "#0096ff")
 
-		//let hint = added_regex.replace("|", " / ").replace(new RegExp(added_regex, "g"), "<font color=#0096ff>**$1**</font>")
-		let hint_array = added_regex.replace(/(\(|\))/g, "").split("|")
-		let hint = this.shuffleArray(hint_array).join(" / ").replace(new RegExp(added_regex, "g"), "<font color=#0096ff>**$1**</font>")
+		//let hint = added_array.replace("|", " / ").replace(new RegExp(added_array, "g"), "<font color=#0096ff>**$1**</font>")
+		let hint = this.shuffleArray(added_array).join(" / ")
+		hint = this.replaceMatchedStringsToGetColor(hint, added_array, "#0096ff")
 		hint = `<br>(${hint})`
 
 		question += ` ${hint}`
-		answer = answer.replace(new RegExp(added_regex, "g"), "<font color=#0096ff>**$1**</font>")
 		question = `[Polish up this in English] ${question}`
+
 		return `%%<br>STARTI [Basic(MD)] ${question} Back: ${answer} %%` + `%% ENDI %%\n`
 	}
 
